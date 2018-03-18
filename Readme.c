d26ffc33748fb7bfbe975b96013513a2cb335974
Aman Verma <amanv1999@gmail.com>
2018-03-18 13:15:23 +0530

    windows created

---
+++ Readme.c
@@ -0,0 +1,20 @@
+#include<GL/glut.h>
+#include<stdio.h>
+  
+
+
+
+
+
+
+int main(int argc,char** argv)
+{
+glutInit(&argc,argv);
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+glutInitWindowSize(1000,1000);
+glutInitWindowPosition(100,100);
+glutCreateWindow("Window for ther Snake Game");
+glutMainLoop();
+}
+
+
