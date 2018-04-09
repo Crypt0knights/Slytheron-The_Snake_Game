f905a7d3b5049e75ac22dcb96365772604401036
Mayank Singh <mayank.singh081997@gmail.com>
2018-04-10 01:13:11 +0530

    CodeBlocks Project Files Added

--- Game CodeBlocks Project/SnakeGame/game.c
+++ Game CodeBlocks Project/SnakeGame/game.c
@@ -1,3 +1,4 @@
+
 //drawing stuff
 #include<GL/gl.h>
 #include<GL/glut.h>
@@ -17,14 +18,21 @@ void initGrid(int x,int y)	//initialize grid
 
 void drawGrid()
 {
-	unit(20,20);
+    int x,y;
+	for(x =0; x<gridX; x++ )
+        {
+            for(y =0; y<gridY; y++)
+            {
+                unit(x, y);
+            }
+        }
 }
 
 void unit(int x,int y)
 {
-	glLineWidth(1.0);
-	glColor3f(1.0,1.0,1.0);
-	glBegin(GL_LINE_LOOP);	//loop of lines, first vertex and last vertex specified will be connected 
+	glLineWidth(0.5);
+	glColor4f(1.0,1.0,1.0,0.1);
+	glBegin(GL_LINE_LOOP);	//loop of lines, first vertex and last vertex specified will be connected
 	glVertex2f(x,y);	//2 floating
 	glVertex2f(x+1,y);
 	glVertex2f(x+1,y+1);
--- Game CodeBlocks Project/SnakeGame/main.c
+++ Game CodeBlocks Project/SnakeGame/main.c
@@ -9,18 +9,22 @@
 #define COLUMNS 40
 #define ROWS 40
 void display_callback();
-void reshape_callback(int,int);	
+void reshape_callback(int,int);
 void init()
 {
-	glClearColor(1.0,0.0,0.0,1.0);	//clear color of the window to red
-	initGrid(COLUMNS,ROWS);		
+
+	glEnable(GL_BLEND);         //added this for maintaing alpha value
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glClearColor(1.0,0.5,0.0,1.0);	//clear color of the window to red
+	initGrid(COLUMNS,ROWS);
 
 }
 int main(int argc, char **argv)
 {
-	glutInit(&argc,argv);	//initialize 
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE /*Doule Buffer Display Window*/);
-	glutInitWindowSize(500,500);
+	glutInit(&argc,argv);	//initialize
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE /*Doule Buffer Display Window*/ | GLUT_DEPTH);
+	glutInitWindowPosition(500,0);
+	glutInitWindowSize(1000,1000);
 	glutCreateWindow("Slytheron-the Snake Game");
 	glutDisplayFunc(display_callback);//register display_callback
 	glutReshapeFunc(reshape_callback);//register reshape callback
@@ -29,7 +33,7 @@ int main(int argc, char **argv)
 	return 0;
 }
 void display_callback()
-{	
+{
 	glClear(GL_COLOR_BUFFER_BIT);//Clearing Color Buffer to get red window
 	drawGrid();
 	glutSwapBuffers();//Buffer is first loaded, another displays
@@ -40,7 +44,7 @@ void reshape_callback(int w, int h)	//after it is resized w/h
 	//typecasting with GLsizei
 	glViewport(0,0,(GLsizei)w,(GLsizei)h);//makes working size after resizing
 	glMatrixMode(GL_PROJECTION/*GL Projection Matrix*/); //setting the co ordinate system
-	glLoadIdentity();//make sure no changes made to matrix 
+	glLoadIdentity();//make sure no changes made to matrix
 	//glOrtho(leftx, rightx, bottom y, top y, zNear, zFar(going inside the screen))	we are working in 0 in zMode
 	glOrtho(0.0,COLUMNS,0.0,ROWS,-1.0,1.0);//sets orthographic projection
 	//Screen with 0,0 in left bottom
