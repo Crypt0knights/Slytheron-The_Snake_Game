2e86f37cd0b9d60a21500f5dd3f60e381aa1e141
Siddhant N Trivedi <31186013+sidntrivedi012@users.noreply.github.com>
2018-04-10 17:36:38 +0530

    Updated game.h

--- Game CodeBlocks Project/SnakeGame/game.h
+++ Game CodeBlocks Project/SnakeGame/game.h
@@ -1,8 +1,15 @@
 #ifndef GAME_H_INCLUDED
 #define GAME_H_INCLUDED
+#define UP 1
+#define DOWN -1
+#define RIGHT 2
+#define LEFT -2
 
 void initGrid(int , int);
 void drawGrid();
 void unit();
+void drawSnake();
+void drawFood();
+void random1(int*,int*);
 
 #endif // GAME_H_INCLUDED
