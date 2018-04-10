43725d90814ae9462e3d9fd4d484060e130c6a7b
Mayank Singh <mayank.singh081997@gmail.com>
2018-04-11 03:22:11 +0530

    Update game.h

--- Game CodeBlocks Project/SnakeGame/game.h
+++ Game CodeBlocks Project/SnakeGame/game.h
@@ -1,15 +1,19 @@
 #ifndef GAME_H_INCLUDED
 #define GAME_H_INCLUDED
+
 #define UP 1
 #define DOWN -1
 #define RIGHT 2
 #define LEFT -2
+//maximum length of snake
+#define MAX 60
 
 void initGrid(int , int);
 void drawGrid();
-void unit();
 void drawSnake();
+void unit();
 void drawFood();
-void random1(int*,int*);
+void random1(int *,int *);
+void delay(int);
 
 #endif // GAME_H_INCLUDED
