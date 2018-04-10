4a21cc8bbab1ae48e9485e1495c36b99db445731
Siddhant N Trivedi <31186013+sidntrivedi012@users.noreply.github.com>
2018-04-10 17:38:09 +0530

    Update main.c

--- Game CodeBlocks Project/SnakeGame/main.c
+++ Game CodeBlocks Project/SnakeGame/main.c
@@ -1,5 +1,3 @@
-//compile using  gcc file.c -o file -lGL -lGLU -lglut
-
 #include<GL/gl.h>
 #include<GL/glut.h>
 #include<stdio.h>
@@ -8,8 +6,14 @@
 
 #define COLUMNS 40
 #define ROWS 40
+#define FPS 10 //10 boxes in one second
+
+extern short sDirection;
+//bool gameOver=false;
+void timer_callback();
 void display_callback();
 void reshape_callback(int,int);
+void keyboard_callback(int);
 void init()
 {
 
@@ -28,15 +32,26 @@ int main(int argc, char **argv)
 	glutCreateWindow("Slytheron-the Snake Game");
 	glutDisplayFunc(display_callback);//register display_callback
 	glutReshapeFunc(reshape_callback);//register reshape callback
+	glutTimerFunc(0,timer_callback,0);
+	glutSpecialFunc(keyboard_callback);
 	init();
 	glutMainLoop();
 	return 0;
 }
+int index=0;//keep track of position
 void display_callback()
 {
 	glClear(GL_COLOR_BUFFER_BIT);//Clearing Color Buffer to get red window
 	drawGrid();
+	drawSnake();
+	drawFood();
 	glutSwapBuffers();//Buffer is first loaded, another displays
+	/*if(gameOver)
+	{
+        MessageBox(0,"Your Score:","GAME OVER",0);
+        exit(0);
+	}*/
+
 }
 void reshape_callback(int w, int h)	//after it is resized w/h
 {	//viewport - rectangular area.
@@ -49,4 +64,32 @@ void reshape_callback(int w, int h)	//after it is resized w/h
 	glOrtho(0.0,COLUMNS,0.0,ROWS,-1.0,1.0);//sets orthographic projection
 	//Screen with 0,0 in left bottom
 	glMatrixMode(GL_MODELVIEW);
+}
+
+void timer_callback()
+{
+    glutPostRedisplay();
+    glutTimerFunc(1000/FPS,timer_callback,0);
+}
+void keyboard_callback(int key)
+{
+    switch(key)
+    {
+        case GLUT_KEY_UP:
+            if(sDirection!=DOWN)
+                sDirection=UP;
+            break;
+        case GLUT_KEY_DOWN:
+            if(sDirection!=UP)
+                sDirection=DOWN;
+            break;
+        case GLUT_KEY_RIGHT:
+            if(sDirection!=LEFT)
+                sDirection=RIGHT;
+            break;
+        case GLUT_KEY_LEFT:
+            if(sDirection!=RIGHT)
+                sDirection=LEFT;
+            break;
+    }
 }
