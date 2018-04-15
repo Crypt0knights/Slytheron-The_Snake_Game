f3e24f285074a6e39bc85227a3e49363c79f2a3a
Siddhant N Trivedi <31186013+sidntrivedi012@users.noreply.github.com>
2018-04-16 00:29:02 +0530

    Added comments to lboard.c

--- Final Project/lboard.c
+++ Final Project/lboard.c
@@ -26,42 +26,40 @@ listsize=0;
 //-----------------GLOBAL VARIABLES--------------------------------------//
 extern int score;               //taking values of score
 //-------------------FUNCTION DEFINITIONS START-----------------------//
-void fileread()
+
+void fileread()					//function to read the previous stored data of scores from the file
 {
     int value,size;value=0;char playerid[10];
     FILE *fp;
     fp=fopen("lb1.txt","r");
-    if (NULL != fp)
+    if (NULL != fp)				//checks if file is empty
     {
         fseek (fp, 0, SEEK_END);
         size = ftell(fp);
         if (0 == size)
         {
-            printf("file is empty\n");
-            fclose(fp);
+            //printf("file is empty\n");
+            fclose(fp);				//closes file if it is empty and jumps out of function.
             return;
         }
         else
         {
-            rewind(fp);
-            //printf("1");
+            rewind(fp);				//brings pointer to starting of file
             while((fscanf(fp,"%s %d ",playerid,&value))!=EOF)
-            {
-                //printf("%d ",value);
-                insertlist(value,playerid);
-                listsize++;
+	    {
+                insertlist(value,playerid);	//calls insertlist to insert the extracted data into the linked list.
+                listsize++;			//calculates the size of the created linked list.
             }
         }
     fclose(fp);
 }
 }
-void insertlist(int val,char playerid[10])
+void insertlist(int val,char playerid[10])	//function to insert the extracted data from file into the linked list.
 {
     node *new_node;
-	new_node=(node*)malloc(sizeof(node));
-	//printf(" %d",val);
-	new_node->score2=val;
-	strcpy(new_node->playername,playerid);
+	new_node=(node*)malloc(sizeof(node));	//allocating memory for saving the extracted data to the Linked list
+	new_node->score2=val;			//copying score
+	strcpy(new_node->playername,playerid);	//copying playername
 	if(first==NULL)
 	{
 		first=new_node;
@@ -77,7 +75,7 @@ void insertlist(int val,char playerid[10])
 	new_node->next=NULL;
     }
 }
-void sortlist()
+void sortlist()				//Implementation of Bubble sort algorithm to sort the linked list in descending order according to score
 {
 node *temp1;
 node *temp2;
@@ -89,9 +87,9 @@ for(i=0;i<n-1;i++)
         temp1=temp1->next;
         for(j=0;j<n-i;j++)
         {
-            if(temp2->score2 <= temp1->score2)
+            if(temp2->score2 <= temp1->score2)		//descending order condition
             {
-                t=temp1->score2;             strcpy(t1,temp1->playername);
+                t=temp1->score2;             strcpy(t1,temp1->playername);		//substituting the score and names simultaneously
                 temp1->score2=temp2->score2; strcpy(temp1->playername,temp2->playername);
                 temp2->score2=t;             strcpy(temp2->playername,t1);
             }
@@ -104,7 +102,7 @@ for(i=0;i<n-1;i++)
     }
 }
 
-void writetofile()
+void writetofile()				//function to write the updated linked list(with the new score) to file.
 {
     FILE *fp;
     fp=fopen("lb1.txt","w");
@@ -114,33 +112,36 @@ void writetofile()
         fprintf(fp,"%s %d ",temp->playername,temp->score2);
         temp=temp->next;
     }
-    fprintf(fp,"%s %d ",temp->playername,temp->score2);
+    fprintf(fp,"%s %d ",temp->playername,temp->score2);	
     fclose(fp);
 }
-void boardprint()
+void boardprint()			//the master function calling other functions and displaying the message boxes.
 {
-        //to update the linked list every time the game starts using the file.
-        fileread();
-        char playerid[10]="MARK";
-        char pos[20];
-        itoa((listsize+1),pos,10);
-        strcat(playerid,pos);
-        insertlist(score,playerid);
-        sortlist();
-        writetofile();
-        char winners[1000];
-        strcpy(winners,"Player ID");
-        strcat(winners,"\t\t");
-        strcat(winners,"SCORE\n\n");
-        node *temp1;
+         fileread();			//fill up the linked list with the previous data if any.
+        char playerid[10]="MARK";	//creating the player ids according to the sequence of game play
+        char pos[20];			//                          " "
+        itoa((listsize+1),pos,10);	//			    " "
+        strcat(playerid,pos);		//			    " "
+        insertlist(score,playerid);	//inserting the new score into the linked list containing the previous gameplay data.
+        sortlist();			//sorting the updated linked list.
+        writetofile();			//writing the all new linked to the file.
+	
+	//DISPLAYING MESSAGE BOXES FROM HERE
+	/*Since message box accepts only String arguments,we will be typecasting every integer(score) stored in the linked 
+	list and then will be appending all of the linked list data into the message box string array which is 'winners'*/
+	
+	char winners[1000];
+        strcpy(winners,"Player ID\t\t");		//copying headings of the leaderboard
+        strcat(winners,"SCORE\n\n");			//	      " "
+        node *temp1;	
         temp1=first;
-        while(temp1->next!=NULL)
+        while(temp1->next!=NULL)			//traversing the linked list and appending data of each node to the string winners.
         {
-            strcat(winners,temp1->playername);
+            strcat(winners,temp1->playername);		//appending the playernames
             strcat(winners,"\t\t");
             char score3[100];
-            itoa(temp1->score2,score3,10);
-            strcat(winners,score3);
+            itoa(temp1->score2,score3,10);		//typecasting the score(int) to String and storing it in score3 char array.
+            strcat(winners,score3);			//appending score.
             strcat(winners,"\n");
             temp1=temp1->next;
         }
@@ -148,14 +149,14 @@ void boardprint()
             strcat(winners,"\t\t");
             char score3[100];
             itoa(temp1->score2,score3,10);
-            strcat(winners,score3);
+            strcat(winners,score3);			//copying details of last node which could not get executed in above loop.
             strcat(winners,"\n");
-        char score1[10];//message box string of score.
+        char score1[10];				//char array to store data of individual score message box.
         itoa(score,score1,10);
         char text[50]={"\t\tOops..!!GAME OVER\t\t\n\n\t\tYOUR SCORE IS- "};
         strcat(text,score1);
-        MessageBox(NULL,text,"GAME OVER",1);
-        MessageBox(NULL,winners,"The Leaderboard",1);
+        MessageBox(NULL,text,"GAME OVER",1);		//printing the individual score message box.
+        MessageBox(NULL,winners,"The Leaderboard",1);	//printing the leaderboard message box.
         exit(0);
 }
 //-------------------------END------------------------------------------------
